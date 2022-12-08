14dbaa5bca94eda04dc3eb2bd8a4e49c3dd6638f
AdamMudrak <mudrets2000@gmail.com>
2022-12-08 22:14:00 +0200

    Declare variables outside methode AND still inside class

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,15 +2,22 @@ package app;
 
 public class Main {
 
-static int e;
+    static int a;
 
-static int f;
+    static int b;
+
+    static int c;
+
+    static int e;
+
+    static int f;
+
     public static void main(String[] args) {
         System.out.println("My app for Users!");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
-        int c = add(a, b);
+        a = 8;
+        b = 2;
+        c = add(a, b);
         System.out.println(c);
         int d = multiply(a, b);
         System.out.println(d);
@@ -28,7 +35,11 @@ static int f;
         return a * b;
     }
 
-    private static int subtract (int a, int b) { return a - b;}
+    private static int subtract(int a, int b) {
+        return a - b;
+    }
 
-    private static int divide(int a, int b) { return a / b; }
+    private static int divide(int a, int b) {
+        return a / b;
+    }
 }
